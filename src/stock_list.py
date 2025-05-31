42da397f45164e771f3f82d7b0fe80eafbf34c30
Little Wiz <codeProjects.gmm@gmail.com>
2025-05-31 08:45:09 +0800

    List all the stocks

--- src/stock_list.py
+++ src/stock_list.py
@@ -1,5 +1,122 @@
 def get_nifty_symbols():
     # Add or update symbols as needed
     return [
-        "TCS.NS", "INFY.NS", "RELIANCE.NS"
+        "HDFCBANK.NS",         # HDFC Bank
+        "ICICIBANK.NS",        # ICICI Bank
+        "SBIN.NS",             # State Bank of India (SBI)
+        "KOTAKBANK.NS",        # Kotak Mahindra Bank
+        "AXISBANK.NS",         # Axis Bank
+        "BAJFINANCE.NS",       # Bajaj Finance
+        "INDUSINDBK.NS",       # IndusInd Bank
+        "IDFCFIRSTB.NS",       # IDFC First Bank
+        "BANDHANBNK.NS",       # Bandhan Bank
+        "FEDERALBNK.NS",       # Federal Bank
+        "CANBK.NS",            # Canara Bank
+        "BANKBARODA.NS",       # Bank of Baroda
+        "PNB.NS",              # Punjab National Bank (PNB)
+        "AUBANK.NS",           # AU Small Finance Bank
+        "RBLBANK.NS",          # RBL Bank
+
+        # IT & Technology
+        "TCS.NS",             # Tata Consultancy Services
+        "INFY.NS",            # Infosys
+        "HCLTECH.NS",         # HCL Technologies
+        "WIPRO.NS",           # Wipro
+        "TECHM.NS",           # Tech Mahindra
+        "LTIM.NS",            # LTIMindtree
+        "MPHASIS.NS",         # Mphasis
+        "PERSISTENT.NS",      # Persistent Systems
+        "COFORGE.NS",         # Coforge
+        "BIRLASOFT.NS",       # Birlasoft
+        "ZENSARTECH.NS",      # Zensar Technologies
+        "SONATSOFTW.NS",      # Sonata Software
+        "HEXAWARE.NS",        # Hexaware Technologies
+        "LTTS.NS",            # L&T Technology Services
+        "FIRSTSOURCE.NS",     # Firstsource Solutions
+
+        # Pharmaceuticals & Healthcare
+        "SUNPHARMA.NS",       # Sun Pharma
+        "DIVISLAB.NS",        # Divi's Laboratories
+        "CIPLA.NS",           # Cipla Ltd
+        "MAXHEALTH.NS",       # Max Healthcare
+        "TORNTPHARM.NS",      # Torrent Pharma
+        "DRREDDY.NS",         # Dr. Reddy's Laboratories
+        "APOLLOHOSP.NS",      # Apollo Hospitals
+        "MANKIND.NS",         # Mankind Pharma
+        "ZYDUSLIFE.NS",       # Zydus Lifesciences
+        "LUPIN.NS",           # Lupin Ltd
+        "AUROPHARMA.NS",      # Aurobindo Pharma
+        "BIOCON.NS",          # Biocon Ltd
+        "GLAND.NS",           # Gland Pharma
+        "ALKEM.NS",           # Alkem Laboratories
+        "GLENMARK.NS",        # Glenmark Pharmaceuticals
+        
+        # Electricals & Consumer Durables
+        "HAVELLS.NS",         # Havells India
+        "DIXON.NS",           # Dixon Technologies
+        "VOLTAS.NS",          # Voltas Ltd
+        "KAYNES.NS",          # Kaynes Technology
+        "BLUESTARCO.NS",      # Blue Star Ltd
+        "WHIRLPOOL.NS",       # Whirlpool
+        "BAJAJELEC.NS",       # Bajaj Electricals
+        "ORIENTELEC.NS",      # Orient Electric
+        "IFBIND.NS",          # IFB Industries
+        "VGUARD.NS",          # V-Guard Industries
+        "SYMPHONY.NS",        # Symphony
+        "TTKPRESTIG.NS",      # TTK Prestige
+        "CROMPTON.NS",        # Crompton Greaves Consumer
+        "EUREKAFORBE.NS",     # Eureka Forbes
+        "SCHAEFFLER.NS",      # Schaeffler India
+
+        # Real Estate
+        "DLF.NS",              # DLF Ltd
+        "GODREJPROP.NS",       # Godrej Properties
+        "OBEROIRLTY.NS",       # Oberoi Realty
+        "PRESTIGE.NS",         # Prestige Estates
+        "BRIGADE.NS",          # Brigade Enterprises
+        "SOBHA.NS",            # Sobha Ltd
+        "PHOENIXLTD.NS",       # Phoenix Mills
+        "MACROTECH.NS",        # Macrotech Developers
+        "ANANTRAJ.NS",         # Anant Raj
+        "PURVA.NS",            # Puravankara
+        "KOLTEPATIL.NS",       # Kolte-Patil Developers
+        "SUNTECK.NS",          # Sunteck Realty
+        "MAHLIFE.NS",          # Mahindra Lifespace Developers
+        "SHOBHA.NS",           # Shobha Developers (duplicate, see SOBHA.NS)
+        "SIGIND.NS",           # Signature Global (India) Ltd
+
+        # Auto & Auto Ancillaries
+        "MARUTI.NS",           # Maruti Suzuki
+        "M&M.NS",              # Mahindra & Mahindra
+        "TATAMOTORS.NS",       # Tata Motors
+        "BAJAJ-AUTO.NS",       # Bajaj Auto
+        "EICHERMOT.NS",        # Eicher Motors
+        "ASHOKLEY.NS",         # Ashok Leyland
+        "HEROMOTOCO.NS",       # Hero MotoCorp
+        "TVSMOTOR.NS",         # TVS Motor
+        "ESCORTS.NS",          # Escorts Kubota
+        "FORCEMOT.NS",         # Force Motors
+        "SMLISUZU.NS",         # SML Isuzu
+        "ATULAUTO.NS",         # Atul Auto
+        "VSTTILLERS.NS",       # V.S.T Tillers Tractors
+        "GREAVESCOT.NS",       # Greaves Cotton
+        "AMARAJABAT.NS",       # Amara Raja Energy & Mobility
+
+        # Metals & Mining
+        "TATASTEEL.NS",        # Tata Steel
+        "JSWSTEEL.NS",         # JSW Steel
+        "HINDALCO.NS",         # Hindalco Industries
+        "VEDL.NS",             # Vedanta Ltd
+        "COALINDIA.NS",        # Coal India
+        "NATIONALUM.NS",       # National Aluminium
+        "NMDC.NS",             # NMDC Ltd
+        "JINDALSTEL.NS",       # Jindal Steel & Power
+        "SAIL.NS",             # Steel Authority of India
+        "HINDZINC.NS",         # Hindustan Zinc
+        "MOIL.NS",             # MOIL Ltd
+        "RATNAMANI.NS",        # Ratnamani Metals & Tubes
+        "WELCORP.NS",          # Welspun Corp
+        "SHYAMMETL.NS",        # Shyam Metalics
+        "LLOYDSME.NS",         # Lloyds Metals & Energy
+
     ]
